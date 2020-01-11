3922982e48ce51a922bfcfe7a5b6ea5136f927e2
Junhong-Kim <jhk1005jhk@gmail.com>
2020-01-12 03:05:19 +0900

    add: Spring Security 설정

---
+++ src/main/java/kim/junhong/springsecurityjwt/controller/SampleController.java
@@ -0,0 +1,15 @@
+package kim.junhong.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping
+    public String sample() {
+        return "Sample";
+    }
+}
